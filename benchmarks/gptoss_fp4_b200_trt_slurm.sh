64b1e92fe6a1e4bb3c9f52b5c42ecd41725cff3f
Jatin Gangani <jgangani@dc2-container-xterm-014.prd.it.nvidia.com>
2025-10-27 15:08:01 -0700

    test

--- benchmarks/gptoss_fp4_b200_trt_slurm.sh
+++ benchmarks/gptoss_fp4_b200_trt_slurm.sh
@@ -76,8 +76,6 @@ set -x
 MAX_NUM_TOKENS=20000
 
 rm -rf ~/.cache/torch_extensions/
-echo "import FLASHINFER"
-python -c 'import flashinfer;print("imported"+flashinfer.__version__);'
 
 # Launch TRT-LLM server
 mpirun -n 1 --oversubscribe --allow-run-as-root \
